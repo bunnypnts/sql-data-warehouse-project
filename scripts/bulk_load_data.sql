18c90eef5b6f27f92a6352fb408bc6ac698d474f
bunnypnts <61928765+bunnypnts@users.noreply.github.com>
2026-06-08 21:03:09 +0800

    Update bulk_load_data.sql

--- scripts/bulk_load_data.sql
+++ scripts/bulk_load_data.sql
@@ -1,71 +1,77 @@
-TRUNCATE TABLE bronze.crm_cust_info;
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\cust_info.csv'
-WITH (
+BEGIN
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.crm_cust_info;
 
-);
+	BULK INSERT bronze.crm_cust_info
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\cust_info.csv'
+	WITH (
 
-TRUNCATE TABLE bronze.crm_sales_details;
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
 
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\sales_details.csv'
-WITH (
+	);
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.crm_sales_details;
 
-);
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\sales_details.csv'
+	WITH (
 
-TRUNCATE TABLE bronze.crm_prd_info;
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
 
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\prd_info.csv'
-WITH (
+	);
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.crm_prd_info;
 
-);
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_crm\prd_info.csv'
+	WITH (
 
-TRUNCATE TABLE bronze.erp_cust_az12;
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\CUST_AZ12.csv'
-WITH (
+	);
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.erp_cust_az12;
 
-);
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\CUST_AZ12.csv'
+	WITH (
 
-TRUNCATE TABLE bronze.erp_loc_a101;
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\LOC_A101.csv'
-WITH (
+	);
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.erp_loc_a101;
 
-);
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\LOC_A101.csv'
+	WITH (
 
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\PX_CAT_G1V2.csv'
-WITH (
+	);
 
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 
-);
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\Jan Mark\OneDrive\Documents\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+
+	);
+
+END
